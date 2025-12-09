bcbdb9996ff48a4dfd7a443f52e22fe8600bf88d
mcyasar <muratcany0167@gmail.com>
2025-12-10 02:10:55 +0300

    feat(menu): implement menu list component with form handling and service integration; add methods for fetching menu items

--- src/app/features/menu/components/menuComponent/menu-list/menu-list.ts
+++ src/app/features/menu/components/menuComponent/menu-list/menu-list.ts
@@ -1,13 +1,67 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MenuItem } from '../menu-item/menu-item';
 import { DailyMenu } from '../daily-menu/daily-menu';
+import { MenuServices } from '../../../services/menuServices/menu-services';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-list',
   imports: [ReactiveFormsModule, MenuItem, DailyMenu],
   templateUrl: './menu-list.html'
 })
-export class MenuList {
+export class MenuList implements OnInit {
+  isModalOpen = false;
+  isLoading: boolean = false;
+  menuListForm!: FormGroup;
+  errorMessage: string = '';
 
+  constructor(
+    private menuService: MenuServices,
+    private formBuilder: FormBuilder,
+    private router : Router,
+    private route: ActivatedRoute,
+    private fb: FormBuilder) {
+    this.menuListForm = this.fb.group({
+      Name: [''],
+      Description: [''],
+      Price: [''],
+      Category: [''],
+      Ingredients: [''],
+      ImageUrl: ['']
+    });
+
+
+  }
+  ngOnInit(): void {
+    this.menuService.getAllMenuItems().subscribe({
+      next: (response) => {
+        console.log('Menu items fetched successfully:', response);
+      },
+      error: (error) => {
+        this.errorMessage = 'Failed to fetch menu items. Please try again later.';
+      }
+    });
+  }
+  getMenuItemsByCategory(category: string) {
+    this.isLoading = true;
+    this.menuService.getMenuItemByCategory(category).subscribe({
+      next: (response) => {
+        console.log(`Menu items for category ${category} fetched successfully:`, response);
+      },
+      error: (error) => {
+        this.errorMessage = `Failed to fetch menu items for category ${category}. Please try again later.`;
+      }});
+    }
+    getMenuItemById(id:string){
+      this.isLoading = true;
+      this.menuService.getMenuItemById(id).subscribe({
+        next: (response) => {
+          console.log('Menu items fetched succesfully:', response);
+        },
+        error: (error) => {
+          this.errorMessage = 'Failed to fetch menu items.';
+        }
+      })
+    }
 }
